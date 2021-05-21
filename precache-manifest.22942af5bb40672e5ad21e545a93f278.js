439b07f2ce93d38bf6d853498580228f3f62fdeb
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-05-21 14:54:09 +0545

    Updates

---
+++ precache-manifest.22942af5bb40672e5ad21e545a93f278.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c4d100c89875363795a0639371302e",
+    "url": "/framer-motion/index.html"
+  },
+  {
+    "revision": "2aae25764d7ad1a070be",
+    "url": "/framer-motion/static/css/main.60c25e8a.chunk.css"
+  },
+  {
+    "revision": "9b88e002efa07b8dc34a",
+    "url": "/framer-motion/static/js/2.ba74b42b.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/framer-motion/static/js/2.ba74b42b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aae25764d7ad1a070be",
+    "url": "/framer-motion/static/js/main.ad737b7f.chunk.js"
+  },
+  {
+    "revision": "339dd82041babf4d5d37",
+    "url": "/framer-motion/static/js/runtime-main.f2361a76.js"
+  }
+]);
