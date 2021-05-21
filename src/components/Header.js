9dfb252db102170d427db0e213ee7d1f68ea3c3a
Rohan Raj Gautam <rohangautamdev@gmail.com>
2021-05-21 14:44:01 +0545

    make logo draggable

--- src/components/Header.js
+++ src/components/Header.js
@@ -29,7 +29,12 @@ const pathVariants = {
 const Header = () => {
   return (
     <header>
-      <div className="logo">
+      <motion.div
+        className="logo"
+        drag
+        dragConstraints={{ left: 0, top: 0, right: 0, bottom: 0 }}
+        dragElastic={2}
+      >
         <motion.svg
           className="pizza-svg"
           xmlns="http://www.w3.org/2000/svg"
@@ -49,7 +54,7 @@ const Header = () => {
             variants={pathVariants}
           />
         </motion.svg>
-      </div>
+      </motion.div>
       <div className="title">
         <motion.h1
           initial={{ y: -100 }}
